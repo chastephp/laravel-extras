470e848ba8acffda56540f9e80df3ae9fb34b7b2
chastephp <chastephp@foxmail.com>
2021-02-21 17:19:49 +0800

    route group api

--- src/Providers/ExtraServiceProvider.php
+++ src/Providers/ExtraServiceProvider.php
@@ -2,12 +2,13 @@
 
 namespace ChastePhp\LaravelExtras\Providers;
 
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
 
 class ExtraServiceProvider extends ServiceProvider
 {
@@ -29,6 +30,26 @@ class ExtraServiceProvider extends ServiceProvider
         Validator::extend('id_no', function ($attribute, $value, $parameters, $validator) {
             return (bool) preg_match("/^(\d{14}|\d{17})(\d{1}|x|X)$/", $value);
         });
+
+        /**
+         * Route a resource to a controller.
+         *
+         * @param  string  $name
+         * @param  string  $controller
+         * @param  array  $options
+         * @return \Illuminate\Routing\PendingResourceRegistration
+         * @static
+         */
+        Route::macro('api', function ($name, $controller, $only = ['list', 'create', 'update', 'info', 'delete']) {
+            $name = rtrim($name, '/').'/';
+            $only = (array) $only;
+            in_array('list', $only) && $this->get($name.'list', join('@', [$controller, 'list']));
+            in_array('create', $only) && $this->post($name.'create', join('@', [$controller, 'create']));
+            in_array('update', $only) && $this->post($name.'update', join('@', [$controller, 'update']));
+            in_array('info', $only) && $this->get($name.'info', join('@', [$controller, 'info']));
+            in_array('delete', $only) && $this->post($name.'delete', join('@', [$controller, 'delete']));
+        });
+
 
         /**
          * success
